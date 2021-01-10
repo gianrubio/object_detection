c3dfba1cf1d71e1e5e4639d474f786939ac298d2
Giancarlo Rubio <grubio@schubergphilis.com>
2021-01-10 19:00:59 -0300

    refactor repo

--- object_detection/colab_tutorials/python-path.py
+++
@@ -1,3 +0,0 @@
-export PYTHONPATH=$PYTHONPATH:/Users/grubio/Downloads/image/multiple-image-detection/models/research
-export PYTHONPATH=$PYTHONPATH:/Users/grubio/Downloads/image/multiple-image-detection/models/research/object_detection
-export PYTHONPATH=$PYTHONPATH:/Users/grubio/Downloads/image/multiple-image-detection/models/research/slim
