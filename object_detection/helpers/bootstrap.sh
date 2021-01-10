0793f3f8d2fb84984f14b71ec0b8698b43bf668b
Giancarlo Rubio <grubio@schubergphilis.com>
2021-01-10 19:39:49 -0300

    fix bootstrap

--- object_detection/helpers/bootstrap.sh
+++ object_detection/helpers/bootstrap.sh
@@ -1,22 +1,27 @@
-!#/bin/bash -xe
-PROJECT_ROOT_PATH=$(pwd)/object_detection
+#!/bin/bash -xe
+PROJECT_ROOT_PATH=${HOME}/object_detection/object_detection
 MODEL=faster_rcnn_inception_resnet_v2_640x640_coco17_tpu-8
 PIPELINE_CONFIG_FILE=pipeline_${MODEL}.config
 
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then    
     apt install git wget -y
 fi
-
+mkdir -p ~/.ssh
 ssh-keyscan github.com >> ~/.ssh/known_hosts 
-git clone https://github.com/gianrubio/object_detection.git
 
-cd $PROJECT_ROOT_PATH 
+git clone --depth 1 https://github.com/gianrubio/object_detection.git
 
+cd $PROJECT_ROOT_PATH/..
 python -m pip install .
+
+cd $PROJECT_ROOT_PATH
+
+ln -s $PROJECT_ROOT_PATH/.. /
 
 wget --quiet http://download.tensorflow.org/models/object_detection/tf2/20200711/$MODEL.tar.gz  
 mkdir -p $PROJECT_ROOT_PATH/trainning/models 
-tar -xzf $MODEL.tar.gz && mv $MODEL $PROJECT_ROOT_PATH/trainning/models 
+tar -xzf $MODEL.tar.gz 
+mv $MODEL $PROJECT_ROOT_PATH/trainning/models 
 rm $MODEL.tar.gz
 
 
@@ -27,8 +32,9 @@ mkdir -p $PROJECT_ROOT_PATH/trainning/assets/
 
 # Partition datase
 python $PROJECT_ROOT_PATH/helpers/preprocessing/partition_dataset.py -i $PROJECT_ROOT_PATH/trainning/images -r 0.15 -x
-for type in "train test"
+for type in $(echo "train test")
 do
-    python $PROJECT_ROOT_PATH/helpers/preprocessing/xml_to_csv.py -i $PROJECT_ROOT_PATH/trainning/images/$type -o $PROJECT_ROOT_PATH/trainning/assets/${type}_labels.csv
-    python $PROJECT_ROOT_PATH/helpers/preprocessing/generate_tfrecord.py --image_dir=$PROJECT_ROOT_PATH/trainning/images/$type --csv_input={PROJECT_ROOT_PATH}/trainning/assets/${type}_labels.csv  --output_path=$PROJECT_ROOT_PATH/trainning/assets/${type}.record
+    echo $type
+    python ${PROJECT_ROOT_PATH}/helpers/preprocessing/xml_to_csv.py -i ${PROJECT_ROOT_PATH}/trainning/images/$type -o ${PROJECT_ROOT_PATH}/trainning/assets/${type}_labels.csv 
+    python ${PROJECT_ROOT_PATH}/helpers/preprocessing/generate_tfrecord.py --image_dir ${PROJECT_ROOT_PATH}/trainning/images/$type --csv_input ${PROJECT_ROOT_PATH}/trainning/assets/${type}_labels.csv  --output_path ${PROJECT_ROOT_PATH}/trainning/assets/${type}.record
 done
