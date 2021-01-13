b998d5ff6cb14cb0218f4008f2b90c4b3fb22208
Giancarlo Rubio <grubio@schubergphilis.com>
2021-01-13 07:34:13 -0300

    add helpers

--- object_detection/helpers/preprocessing/bootstrap.sh
+++ object_detection/helpers/preprocessing/bootstrap.sh
@@ -5,25 +5,7 @@ export PROJECT_ROOT_PATH=${HOME}/object_detection/object_detection
 export MODEL=ssd_resnet50_v1_fpn_640x640_coco17_tpu-8
 export PIPELINE_CONFIG_FILE=pipeline_${MODEL}.config
 
-if [[ "$OSTYPE" == "linux-gnu"* ]]; then    
-    apt install git wget -y
-fi
-mkdir -p ~/.ssh
-ssh-keyscan github.com >> ~/.ssh/known_hosts 
-
-git clone --depth 1 https://github.com/gianrubio/object_detection.git
-
 cd $PROJECT_ROOT_PATH
-cp $PROJECT_ROOT_PATH/setup.py .
-python -m pip install .
-
-
-wget --quiet http://download.tensorflow.org/models/object_detection/tf2/20200711/$MODEL.tar.gz  
-mkdir -p $PROJECT_ROOT_PATH/trainning/models 
-tar -xzf $MODEL.tar.gz 
-mv $MODEL $PROJECT_ROOT_PATH/trainning/models 
-rm $MODEL.tar.gz
-
 
 rm -rf $PROJECT_ROOT_PATH/trainning/images/test 
 rm -rf $PROJECT_ROOT_PATH/trainning/images/train
